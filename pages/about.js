91f67a4af763188fe68cfac947a1ac4233ec5534
Donovan van Heerden <dp.vanheerden@gmail.com>
2022-01-23 16:25:50 +0200

    push about contents

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout, { siteTitle } from '../components/layout'
+
+export default function About() {
+  return (
+    <Layout>
+      <Head>About</Head>
+      <article>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis. Facilisi cras fermentum odio eu feugiat pretium nibh ipsum consequat. Duis at tellus at urna condimentum mattis pellentesque id nibh. Tempor id eu nisl nunc mi ipsum faucibus. Nunc non blandit massa enim nec dui nunc. Nullam vehicula ipsum a arcu. Integer malesuada nunc vel risus commodo viverra maecenas. At quis risus sed vulputate odio ut enim. Venenatis urna cursus eget nunc scelerisque viverra mauris. Auctor urna nunc id cursus metus aliquam.</p>
+        <p>Vel pharetra vel turpis nunc eget lorem dolor sed viverra. Tortor aliquam nulla facilisi cras fermentum odio. Posuere urna nec tincidunt praesent semper feugiat nibh sed. Volutpat lacus laoreet non curabitur gravida arcu ac tortor. Sollicitudin aliquam ultrices sagittis orci a scelerisque. Commodo odio aenean sed adipiscing diam. Lorem ipsum dolor sit amet. Velit dignissim sodales ut eu sem integer vitae justo eget. Viverra mauris in aliquam sem. Convallis convallis tellus id interdum velit laoreet id donec ultrices. Dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique. Et tortor at risus viverra adipiscing at.</p>
+        <p>Ullamcorper eget nulla facilisi etiam dignissim diam quis enim. Magna fringilla urna porttitor rhoncus dolor purus non enim praesent. Rhoncus urna neque viverra justo nec ultrices. Vitae congue eu consequat ac. Arcu risus quis varius quam quisque id diam vel. Fermentum iaculis eu non diam phasellus. Cras ornare arcu dui vivamus arcu felis bibendum ut. Cursus in hac habitasse platea. Massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin. Blandit massa enim nec dui nunc. In tellus integer feugiat scelerisque varius morbi. Viverra orci sagittis eu volutpat odio.</p>
+      </article>
+    </Layout>
+  )
+}
